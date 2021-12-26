e54ec3086046e6c728854a238bd0121c249639c4
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2021-12-26 21:37:32 +0530

    pageone

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,66 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <h1>COMING </h1>
+    <h1>SOON </h1>
+    <p>We will be celebrating the launch of our new site very soon !</p>
+    <v-btn
+     class="mx-2"
+      color="#80DEEA"
+    >
+      Notify Me !
+    </v-btn>
+    <footer>
+      <p> &#174; 2023 by"Coming Soon " . Proudly Created with <u>Webilicious.com</u></p>
+    <div class="icons">
+      <v-icon
+        medium
+      >
+       mdi-facebook
+      </v-icon>
+      <v-icon
+        medium
+      >
+       mdi-twitter
+      </v-icon>
+      <v-icon
+        medium
+      >
+         mdi-instagram
+      </v-icon>
+    </div>
+   
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+name:"HelloWorld",
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,500&display=swap');
+
+.container{
+  font-family: 'Poppins', sans-serif;
+  text-align: center;
+  margin-top: 120px;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+h1{
+  font-size: 60px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+p{
+  padding: 30px;
+  font-size: 20px;
 }
-a {
-  color: #42b983;
+
+footer{
+  margin-top: 150px;
+}
+.icons{
+  padding-left: 15px;
 }
 </style>
